63fba34ae4cdca2afae6848981edb6d2c2aa7771
wscffaa <1294714904@qq.com>
2020-03-20 09:46:51 +0800

    2.2.1 Python range函数

---
+++ 2.2.1/range.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/20 9:38
+# @Author  : wscffaa
+# @Email   : 1294714904@qq.com
+# @File    : range.py
+# @Software: PyCharm
+
+#range()有三种语法表达形式：
+     #range(start,end,step=1)  step是步长，这里为1
+     #range(start,end)    默认step为1
+     #range(end)    默认start为0，step=1
+
+print(list(range(3,21,2)))
+print(list(range(3,21)))
+print(list(range(21)))
+
